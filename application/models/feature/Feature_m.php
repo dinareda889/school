c8e2d8cafef76872063d9ff2c50242796888410b
omniaAbdallah <omnia.ali36036@yahoo.com>
2023-12-17 13:48:10 +0200

    feature dash

---
+++ application/models/feature/Feature_m.php
@@ -0,0 +1,78 @@
+<?php
+class Feature_m extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function insert()
+    {
+
+        /*echo "<pre>";
+        print_r($_POST);
+        echo "</pre>";
+        die;*/
+        $data['description_ar'] = $this->input->post('description_ar', true);
+        $data['description_en'] = $this->input->post('description_en', true);
+        $data['title_ar'] = $this->input->post('title_ar', true);  
+        $data['title_en'] = $this->input->post('title_en', true);
+        $data['icon'] = $this->input->post('icon', true);
+        $data['date_ar'] = date("Y-m-d");
+        $data['date'] = strtotime(date("Y-m-d"));
+        $data['publisher'] = $_SESSION['user_id'];
+        $data['publisher_name'] = $_SESSION['user_name'];
+      
+
+        $this->db->insert('feature', $data);
+    }
+
+    public function fetch_feature()
+    {
+        $this->db->select('*');
+        $this->db->from('feature');
+        $this->db->order_by('id', 'DESC');
+        $query = $this->db->get();
+        if ($query->num_rows() > 0) {
+            $i = 0;
+            foreach ($query->result() as $row) {
+                $data[$i] = $row;
+                $i++;
+            }
+            return $data;
+        }
+        return false;
+    }
+
+    public function delete($id)
+    {
+        
+        $this->db->where('id', $id);
+        $this->db->delete('feature');
+    }
+
+    public function get_by_id($id)
+    {
+        $this->db->where('id', $id);
+        $query = $this->db->get_where("feature", array('id' => $id));
+        if ($query->num_rows() > 0) {
+            return $query->row();
+        } else {
+            return false;
+        }
+    }
+
+    public function update($id)
+    {
+        $data['title_ar'] = $this->input->post('title_ar', true);  
+        $data['title_en'] = $this->input->post('title_en', true);
+        $data['description_ar'] = $this->input->post('description_ar', true);
+        $data['description_en'] = $this->input->post('description_en', true);
+        $data['icon'] = $this->input->post('icon', true);
+        $data['date_ar'] = date("Y-m-d");
+        $data['date'] = strtotime(date("Y-m-d"));
+        $data['publisher'] = $_SESSION['user_id'];
+        $data['publisher_name'] = $_SESSION['user_name'];
+        $this->db->where('id', $id);
+        $this->db->update('feature', $data);
+    }
+} // END CLASS
---
+++ application/views/feature/all_feature.php
@@ -0,0 +1,101 @@
+<div class="content-wrapper" style="min-height: 921px;">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            <?= $title ?>
+            <small></small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="<?= base_url() . 'Dash' ?>"><i class="fa fa-dashboard"></i> الرئيسية</a></li>
+            <li class="active">كل الخصائص </li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title"><?= $title ?></h3>
+                    </div><!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+
+                            <div class="col-sm-12">
+                                <table id="all_data" class="display example " style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-center">م</th>
+                                            <th class="text-center">الاسم بالعربية</th>
+                                            <th class="text-center">الاسم بالانجليزية</th>
+                                            
+                                            <th class="text-center">التحكم </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        if (isset($all_feature) && $all_feature != null) {
+                                            $x = 0;
+                                            foreach ($all_feature as $record) { ?>
+
+                                        <tr>
+                                            <td><?php echo ($x + 1); ?></td>
+                                            <td><?php echo $record->title_ar; ?> </td>
+                                            <td><?php echo $record->title_en; ?></td>
+                                           
+                                            
+                                            <td>
+                                                <a class="btn btn-info btn-sm mr-3 mb-3" href="#" onclick='swal({
+                                                            title: "هل انت متأكد من التعديل ؟",
+                                                            text: "",
+                                                            type: "warning",
+                                                            showCancelButton: true,
+                                                            confirmButtonClass: "btn-warning",
+                                                            confirmButtonText: "تعديل",
+                                                            cancelButtonText: "إلغاء",
+                                                            closeOnConfirm: false
+                                                            },
+                                                            function(){
+
+                                                            window.location="<?php echo base_url(); ?>feature/Feature_c/update/<?php echo base64_encode($record->id); ?>";
+                                                            });'>
+                                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                                                <a class="btn btn-danger btn-sm mr-3 mb-3" href="#" onclick='swal({
+                                                            title: "تنبيه",
+                                                            text: "هل انت متأكد ن الحذف",
+                                                            type: "warning",
+                                                            showCancelButton: true,
+                                                            confirmButtonClass: "btn-danger",
+                                                            confirmButtonText: "حذف",
+                                                            cancelButtonText: "إلغاء",
+                                                            closeOnConfirm: false
+                                                            },
+                                                            function(){
+                                                            swal("تم الحذف!", "", "success");
+                                                            window.location="<?php echo base_url(); ?>feature/Feature_c/delete/<?php echo base64_encode($record->id); ?>";
+                                                            });'>
+                                                    <i class="fa fa-trash"> </i>
+                                                </a>
+                                            </td>
+
+
+                                        </tr>
+                                        <?php $x++;
+                                            }
+                                        } else {
+                                            echo "لا يوجد بنرات ";
+                                        } ?>
+
+
+                                    </tbody>
+
+                                </table>
+                            </div>
+                        </div>
+                    </div><!-- /.box-body -->
+                </div><!-- /.box -->
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </section><!-- /.content -->
+</div>
